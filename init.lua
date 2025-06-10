c062e8b0e87c528287d12f7357b43863d243e7d3
Joel Eriksson <knickedixen@gmail.com>
2025-06-10 15:22:19 +0200

    some configs

--- init.lua
+++ init.lua
@@ -296,11 +296,12 @@ require('lazy').setup({
         -- You can put your default mappings / updates / etc. in here
         --  All the info you're looking for is in `:help telescope.setup()`
         --
-        -- defaults = {
-        --   mappings = {
-        --     i = { ['<c-enter>'] = 'to_fuzzy_refine' },
-        --   },
-        -- },
+        defaults = {
+          mappings = {
+            -- KNIX: Add ctrl-d binding to delete buffers in buffer list
+            i = { ['<c-d>'] = require('telescope.actions').delete_buffer },
+          },
+        },
         -- pickers = {}
         extensions = {
           ['ui-select'] = {
@@ -627,7 +628,8 @@ require('lazy').setup({
         -- python = { "isort", "black" },
         --
         -- You can use 'stop_after_first' to run the first available formatter from the list
-        -- javascript = { "prettierd", "prettier", stop_after_first = true },
+        javascript = { 'prettierd', 'prettier', stop_after_first = true },
+        json = { 'prettierd', 'prettier', stop_after_first = true },
       },
     },
   },
@@ -884,6 +886,66 @@ require('lazy').setup({
 -- vim: ts=2 sts=2 sw=2 et
 
 -- knix:
+
+local state = {
+  floating = {
+    buf = -1,
+    win = -1,
+  },
+}
+
+local function create_floating_window(opts)
+  opts = opts or {}
+  local width = opts.width or math.floor(vim.o.columns * 0.8)
+  local height = opts.height or math.floor(vim.o.lines * 0.8)
+
+  -- Calculate the position to center the window
+  local col = math.floor((vim.o.columns - width) / 2)
+  local row = math.floor((vim.o.lines - height) / 2)
+
+  -- Create a buffer
+  local buf = nil
+  if vim.api.nvim_buf_is_valid(opts.buf) then
+    buf = opts.buf
+  else
+    buf = vim.api.nvim_create_buf(false, true) -- No file, scratch buffer
+  end
+
+  -- Define window configuration
+  local win_config = {
+    relative = 'editor',
+    width = width,
+    height = height,
+    col = col,
+    row = row,
+    style = 'minimal', -- No borders or extra UI elements
+    border = 'rounded',
+  }
+
+  -- Create the floating window
+  local win = vim.api.nvim_open_win(buf, true, win_config)
+
+  return { buf = buf, win = win }
+end
+
+local toggle_terminal = function()
+  if not vim.api.nvim_win_is_valid(state.floating.win) then
+    state.floating = create_floating_window { buf = state.floating.buf }
+    if vim.bo[state.floating.buf].buftype ~= 'terminal' then
+      vim.cmd.terminal()
+    end
+  else
+    vim.api.nvim_win_hide(state.floating.win)
+  end
+end
+
+vim.api.nvim_create_user_command('Floaterminal', toggle_terminal, {})
+vim.keymap.set('t', '<esc><esc>', '<c-\\><c-n>')
+vim.keymap.set('n', '<C-t>', '<cmd>Floaterminal<CR>')
+vim.keymap.set('t', '<C-t>', '<cmd>Floaterminal<CR>')
+
+-- Always open terminals in insert
+vim.api.nvim_create_autocmd({ 'TermOpen', 'BufEnter' }, { pattern = 'term://*', command = 'startinsert' })
 
 vim.keymap.set('n', '<leader>xp', '<cmd>w<CR> <cmd>!python3 %:p<CR>')
 vim.keymap.set('n', '<leader>xl', ':.lua<CR>')
--- lua/custom/plugins/init.lua
+++ lua/custom/plugins/init.lua
@@ -2,4 +2,61 @@
 --  I promise not to create any merge conflicts in this directory :)
 --
 -- See the kickstart.nvim README for more information
-return {}
+return {
+  {
+    'knickedixen/ask-claude.nvim',
+    dir = '/home/knickedixen/workspace/nvim-plugins/ask-claude.nvim/',
+    config = function()
+      vim.keymap.set('n', '<C-a>', '<cmd>AskClaude<CR>')
+      vim.keymap.set('c', '<C-a>', '<cmd>AskClaude<CR>')
+      vim.keymap.set('t', '<C-a>', '<cmd>AskClaude<CR>')
+      vim.keymap.set('n', '<leader>ac', '<cmd>AskClaudeContext<CR>')
+      vim.keymap.set('n', '<leader>ad', '<cmd>AskClaudeDiagnostics<CR>')
+      vim.keymap.set('v', '<leader>ad', '<cmd>AskClaudeDiagnostics<CR>')
+      vim.keymap.set('v', '<leader>ae', '<cmd>AskClaudeContext<CR>')
+    end,
+  },
+  {
+    'MeanderingProgrammer/render-markdown.nvim',
+    dependencies = { 'nvim-treesitter/nvim-treesitter', 'echasnovski/mini.nvim' }, -- if you use the mini.nvim suite
+    -- dependencies = { 'nvim-treesitter/nvim-treesitter', 'echasnovski/mini.icons' }, -- if you use standalone mini plugins
+    -- dependencies = { 'nvim-treesitter/nvim-treesitter', 'nvim-tree/nvim-web-devicons' }, -- if you prefer nvim-web-devicons
+    ---@module 'render-markdown'
+    ---@type render.md.UserConfig
+    opts = {},
+  },
+  {
+    'luukvbaal/statuscol.nvim',
+    config = function()
+      -- Custom function to show both absolute and relative line numbers
+      local function lnum_both()
+        local lnum = vim.v.lnum
+        local relnum = vim.v.lnum == vim.fn.line '.' and 0 or math.abs(vim.v.lnum - vim.fn.line '.')
+        return string.format('%3d %2d', lnum, relnum)
+      end
+      require('statuscol').setup {
+        setopt = true,
+        segments = {
+          {
+            sign = {
+              namespace = { 'gitsigns.*' },
+              name = { 'gitsigns.*' },
+            },
+          },
+          {
+            sign = {
+              namespace = { '.*' },
+              name = { '.*' },
+              auto = true,
+            },
+          },
+          {
+            text = { lnum_both, ' ' },
+            condition = { true },
+            click = 'v:lua.ScLa',
+          },
+        },
+      }
+    end,
+  },
+}
